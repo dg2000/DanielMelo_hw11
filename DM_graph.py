43058febe2f8036d41e6e284700a411258adc4ff
AdminBiocomputo <dg.melo@uniandes.edu.co>
2018-04-17 16:53:55 -0500

    Ya

--- DM_graph.py
+++ DM_graph.py
@@ -10,14 +10,23 @@ t = np.loadtxt("tray.txt")[:, 2]
 
 plt.plot(t, x, label = "Posicion vs Tiempo")
 plt.legend(loc=0)
+plt.xlabel("Tiempo")
+plt.ylabel("Posicion")
+plt.title("X vs t")
 plt.savefig("pos.png")
 
 plt.figure()
 plt.plot(t, v, label = "Velocidad vs Tiempo")
+plt.xlabel("Tiempo")
+plt.ylabel("Velocidad")
+plt.title(" V vs t ")
 plt.legend(loc=0)
 plt.savefig("vel.png")
 
 plt.figure()
 plt.plot(x, v, label = "Velocidad vs Posicion")
+plt.xlabel("Posicion")
+plt.ylabel("Velocidad")
+plt.title(" V vs x ")
 plt.legend(loc=0)
 plt.savefig("phase.png")
